e48462a80face454175973e01ec536fa16ff5306
Grant Wenzlau <grantwenzlau@gmail.com>
2013-07-01 19:14:45 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zuzu::Application.config.secret_token = '04becf182e1c7e800d12e9eb50e30b3b8b42eec9763844dda5124d39580365dfc509a5ab9f4bf5f127c0c52a5e746f4052ce18a80eef7f46023492ae30c7f82e'
